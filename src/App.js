3e775feb2e87ce24f14f2e69b229503ac6b5a9db
Sree <pmanjusree31@gmail.com>
2025-01-19 01:02:25 -0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-
-function App() {
+export default function Square() {
     return (
-        <div>
-            <h1>Hello, World!</h1>
-        </div>
+        <>
+            <div className="board-row">
+                <button className="square">1</button>
+                <button className="square">2</button>
+                <button className="square">3</button>
+            </div>
+            <div className="board-row">
+                <button className="square">4</button>
+                <button className="square">5</button>
+                <button className="square">6</button>
+            </div>
+            <div className="board-row">
+                <button className="square">7</button>
+                <button className="square">8</button>
+                <button className="square">9</button>
+            </div>
+        </>
     );
 }
-
-export default App;
